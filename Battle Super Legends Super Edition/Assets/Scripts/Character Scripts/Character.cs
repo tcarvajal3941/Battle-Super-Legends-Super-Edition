9a20b5587d16b586782bd942120648a6543c751c
rileymburton <35310092+rileymburton@users.noreply.github.com>
2018-09-10 16:19:31 -0700

    Scripts added
    Character class and CharactersDatabase script added

---
+++ Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
