16642d555f7a4340125395c54cec8523cbfc4398
rileymburton <35310092+rileymburton@users.noreply.github.com>
2018-09-10 21:21:08 -0700

    Updated Character Scripts
    added sonic pizzas example

--- Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/Character.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/Character.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour 
+{
+	public int characterHealth { get; set; }
+	public int specialBar { get; set; }
+	public float forwardSpeed { get; set; }
+	public float backwardSpeed { get; set; }
+	public string characterName { get; set; }
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
+	void Start () 
+	{
+		//check if any of the properties are null. If they are, throw a Debug.LogError
+	}	
 }
--- Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/CharactersDatabase.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/CharactersDatabase.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharactersDatabase : MonoBehaviour {
+public class CharactersDatabase : MonoBehaviour 
+{
+	public Character sonicPizzas = new Character();
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-		
+	void Start () 
+	{
+		//sonic pizzas
+		sonicPizzas.characterHealth = 100;
+		sonicPizzas.specialBar = 0;
+		sonicPizzas.forwardSpeed = 10f;
+		sonicPizzas.backwardSpeed = 5f;
+		sonicPizzas.characterName = "Sonic Pizzas";
 	}
 }
