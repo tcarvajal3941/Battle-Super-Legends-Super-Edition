b582c6f552e02e347ebfc4c50a4dffe435e2858a
rileymburton <35310092+rileymburton@users.noreply.github.com>
2018-09-12 09:18:50 -0700

    Added man with hat 9/12/18
    changed some things to character script and added man with hat

--- Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/CharactersDatabase.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/Character Scripts/CharactersDatabase.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CharactersDatabase : MonoBehaviour 
 {
 	public Character sonicPizzas = new Character();
+	public Character manWithHat = new Character();
 
 	void Start () 
 	{
 		//sonic pizzas
-		sonicPizzas.characterHealth = 100;
+		sonicPizzas.health = 100;
 		sonicPizzas.specialBar = 0;
 		sonicPizzas.forwardSpeed = 10f;
 		sonicPizzas.backwardSpeed = 5f;
 		sonicPizzas.characterName = "Sonic Pizzas";
+
+		//man with hat
+		manWithHat.health = 100;
+		manWithHat.specialBar = 0;
+		manWithHat.forwardSpeed = 10f;
+		manWithHat.backwardSpeed = 5f;
+		manWithHat.characterName = "Man with Hat";
 	}
 }
