d0e76765c3849ecdd5ac6879a0f5d4ab99a904d6
granthendrickson19 <31490789+granthendrickson19@users.noreply.github.com>
2018-09-18 09:57:05 -0700

    Settings menu work 9/18/2018
    added settings menu with working fullscreen toggle, resolution changer, and volume sliders. working on key bindings

--- Battle Super Legends Super Edition/Assets/Scripts/settingsController.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/settingsController.cs
@@ -2,12 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class settingsController : MonoBehaviour {
 
-	public AudioMixer audioMixer;
+	public AudioMixer masterAudioMixer;
+	public AudioMixer musicAudioMixer;
+	public AudioMixer sfxAudioMixer;
 
-	public void setVolume(float volume){
-		audioMixer.SetFloat("volumeMaster", volume);
+	Resolution[] resolutions;
+
+	public Dropdown resolutionDropdown;
+	void Start(){
+		resolutions = Screen.resolutions;
+
+		resolutionDropdown.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		int currentResolutionIndex = 0;
+		for(int i = 0; i < resolutions.Length; i++){
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+			options.Add(option);
+
+			if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+				currentResolutionIndex = i;
+			}
+		}
+
+		resolutionDropdown.AddOptions(options);
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
 	}
+
+	public void SetResolution (int resolutionIndex){
+		
+		Resolution resolution = resolutions[resolutionIndex];
+		Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+	}
+
+	public void setMasterVolume(float volume){
+		masterAudioMixer.SetFloat("volumeMaster", volume);
+	}
+		public void setMusicVolume(float volume){
+		musicAudioMixer.SetFloat("volumeMusic", volume);
+	}
+		public void setSFXVolume(float volume){
+		sfxAudioMixer.SetFloat("volumeSFX", volume);
+	}
+
+		public void setFullScreen(bool isFullscreen){
+			Screen.fullScreen = isFullscreen;
+		}
+
+		public void returnToMenu(){
+			SceneManager.LoadScene(0);
+		}
 }
