e34a3205a02b67e2fb4804a53571663b46743e3e
granthendrickson19 <31490789+granthendrickson19@users.noreply.github.com>
2018-09-26 10:01:31 -0700

    9/26/18
    Finished all of the methods in Keybinding script adding a check that if any of the keybinds are null you can't leave the menu until you set all keybinds to a key

--- Battle Super Legends Super Edition/Assets/Scripts/SceneLoader.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/SceneLoader.cs
@@ -11,8 +11,17 @@ public class SceneLoader : MonoBehaviour {
 		SceneManager.LoadScene(1);
 	}
 
-	public void startSettings(){
-		SceneManager.LoadScene(2);
+	public IEnumerator startSettings(){
+		Transform panel =  transform.Find("keyPanel");
+		panel.gameObject.SetActive(false);
+		if(KeybindingsScript.Kb.right != KeyCode.None && KeybindingsScript.Kb.left != KeyCode.None && KeybindingsScript.Kb.jump != KeyCode.None && KeybindingsScript.Kb.crouch != KeyCode.None && KeybindingsScript.Kb.lightAttack != KeyCode.None && KeybindingsScript.Kb.mediumAttack != KeyCode.None && KeybindingsScript.Kb.heavyAttack != KeyCode.None && KeybindingsScript.Kb.uniqueAttack != KeyCode.None){
+			SceneManager.LoadScene(2);
+		}
+		else{
+			panel.gameObject.SetActive(true);
+			yield return new WaitForSeconds(2);
+			panel.gameObject.SetActive(false);
+		}
 	}
 	public void startMapSelect(){
 
