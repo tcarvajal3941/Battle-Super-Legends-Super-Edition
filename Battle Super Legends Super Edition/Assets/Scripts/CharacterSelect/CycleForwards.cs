c036f2ea38369a4552b06a14b39a73a681dddd11
rileymburton <35310092+rileymburton@users.noreply.github.com>
2018-10-11 09:57:13 -0700

    Character Select Screen Updates 10/11

---
+++ Battle Super Legends Super Edition/Assets/Scripts/CharacterSelect/CycleForwards.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleForwards : MonoBehaviour {
+
+	private int i;
+	private SpriteRenderer sr;
+	public GameObject characterSelected;
+
+	[SerializeField]
+	private Sprite[] sprites; 
+	
+
+	public void CycleCharacterForwards()
+	{
+		sr = characterSelected.GetComponent<SpriteRenderer>();
+		for(i = 0; i < sprites.Length; i++)
+		{
+			sr.sprite = sprites[i]; //finsih this
+		}
+	}
+}
