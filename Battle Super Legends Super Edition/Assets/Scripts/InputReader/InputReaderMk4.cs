99e2de5ada221c0a24f5c5858de1abb46a9e48bf
Sebastian Miller <SMiller7644@student.pps.net>
2018-10-22 09:36:09 -0700

    fixed key references?

--- Battle Super Legends Super Edition/Assets/Scripts/InputReader/InputReaderMk4.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/InputReader/InputReaderMk4.cs
@@ -8,29 +8,13 @@ public class InputReaderMk4 : MonoBehaviour {
 	public int  inputDirection;
 	public int  inputButton;
 
-	public KeyCode jump;
-	public KeyCode down;
-	public KeyCode left;
-	public KeyCode right;
-	public KeyCode light;
-	public KeyCode med;
-	public KeyCode heavy;
-	public KeyCode unique;
-
 	// Use this for initialization
 	void Start () {
 		facingOpponent = true;
 		inputDirection = 5;
 		inputButton    = 0;
 
-		jump   = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("jumpkey", "W"));
-		down   = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("crouchkey", "S"));
-		left   = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("leftkey", "A"));
-		right  = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("rightkey", "D"));
-		light  = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("lightattackkey", "Y"));
-		med    = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("mediumattackkey", "U"));
-		heavy  = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("heavyattackkey", "I"));
-		unique = (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("uniqueattackkey", "K"));
+		Debug.Log(Input.GetKey(KeybindingsScript.Kb.jump));
 	}
 	
 	// Update is called once per frame
@@ -47,93 +31,90 @@ public class InputReaderMk4 : MonoBehaviour {
 		if (facingOpponent == false)
 		{
 			//walk to the left
-			if (Input.GetKey(left))
+			if (Input.GetKey(KeybindingsScript.Kb.left))
         	{
-				Debug.Log("Walking");
 				inputDirection = 6;
         	}
 			//walk to the right
-        	if (Input.GetKey(right))
+        	if (Input.GetKey(KeybindingsScript.Kb.right))
         	{
-				Debug.Log("Jumping");
 				inputDirection = 4;
         	}
 
 			//grounded jump
-			if (Input.GetKeyDown(jump))
+			if (Input.GetKey(KeybindingsScript.Kb.jump))
         	{
-				Debug.Log("Jumping");
 				inputDirection = 8;
 			}
-			if (Input.GetKey(jump) && Input.GetKeyDown(left))
+			if (Input.GetKey(KeybindingsScript.Kb.jump) && Input.GetKey(KeybindingsScript.Kb.left))
 			{
 				inputDirection = 9;
 			}
-			if (Input.GetKey(jump) && Input.GetKeyDown(right))
+			if (Input.GetKey(KeybindingsScript.Kb.jump) && Input.GetKey(KeybindingsScript.Kb.right))
 			{
 				inputDirection = 7;
 			}
 			
 			//crouch
-        	if (Input.GetKey(down))
+        	if (Input.GetKey(KeybindingsScript.Kb.crouch))
         	{
-				Debug.Log("Crouching");
 				inputDirection = 2;
 			}
-			if (Input.GetKey(down) && Input.GetKey(left))
+			if (Input.GetKey(KeybindingsScript.Kb.crouch) && Input.GetKey(KeybindingsScript.Kb.left))
 			{
-				Debug.Log("Crouching");
 				inputDirection = 3;
 			}
-			if (Input.GetKey(down) && Input.GetKey(right))
+			if (Input.GetKey(KeybindingsScript.Kb.left) && Input.GetKey(KeybindingsScript.Kb.right))
 			{
-				Debug.Log("Crouching");
 				inputDirection = 1;
 			}
 		}
 		if (facingOpponent == true)
 		{
 			//walk to the left
-			if (Input.GetKey(left))
+			if (Input.GetKey(KeybindingsScript.Kb.left))
         	{
 				inputDirection = 4;
         	}
 			//walk to the right
-        	if (Input.GetKey(right))
+        	if (Input.GetKey(KeybindingsScript.Kb.right))
         	{
 				inputDirection = 6;
         	}
 
 			//grounded jump
-			if (Input.GetKeyDown(jump))
+			if (Input.GetKey(KeybindingsScript.Kb.jump))
         	{
 				inputDirection = 8;
 			}
-			if (Input.GetKey(jump) && Input.GetKeyDown(left))
+			if (Input.GetKey(KeybindingsScript.Kb.jump) && Input.GetKey(KeybindingsScript.Kb.left))
 			{
 				inputDirection = 7;
 			}
-			if (Input.GetKey(jump) && Input.GetKeyDown(right))
+			if (Input.GetKey(KeybindingsScript.Kb.jump) && Input.GetKey(KeybindingsScript.Kb.right))
 			{
 				inputDirection = 9;
 			}
 			
 			//crouch
-        	if (Input.GetKey(down))
+        	if (Input.GetKey(KeybindingsScript.Kb.crouch))
         	{
 				inputDirection = 2;
 			}
-			if (Input.GetKey(down) && Input.GetKey(left))
+			if (Input.GetKey(KeybindingsScript.Kb.crouch) && Input.GetKey(KeybindingsScript.Kb.left))
 			{
 				inputDirection = 1;
 			}
-			if (Input.GetKey(down) && Input.GetKey(right))
+			if (Input.GetKey(KeybindingsScript.Kb.crouch) && Input.GetKey(KeybindingsScript.Kb.right))
 			{
 				inputDirection = 3;
 			}
 
 			//return to idle
-			if (!Input.anyKeyDown)
+			if (!Input.GetKey(KeybindingsScript.Kb.left) || !Input.GetKey(KeybindingsScript.Kb.right) ||
+				!Input.GetKey(KeybindingsScript.Kb.jump) || !Input.GetKey(KeybindingsScript.Kb.crouch) ||
+				!Input.GetKey(KeybindingsScript.Kb.left) && !Input.GetKey(KeybindingsScript.Kb.right) ||
+				!Input.GetKey(KeybindingsScript.Kb.jump) && !Input.GetKey(KeybindingsScript.Kb.crouch))
 			{
 				inputDirection = 5;
 			}
@@ -144,39 +125,45 @@ public class InputReaderMk4 : MonoBehaviour {
 	public int getButton()
 	{
 		inputButton = 0;
-		if (Input.GetKey(light) || Input.GetKey(light) && Input.GetKey(unique))
+		if (Input.GetKey(KeybindingsScript.Kb.lightAttack) || 
+			Input.GetKey(KeybindingsScript.Kb.lightAttack) && Input.GetKey(KeybindingsScript.Kb.uniqueAttack))
 		{
 			inputButton = 1;//Light (A)
 		}
-		if (Input.GetKey(med) || Input.GetKey(med) && Input.GetKey(unique))
+		if (Input.GetKey(KeybindingsScript.Kb.mediumAttack) || 
+			Input.GetKey(KeybindingsScript.Kb.mediumAttack) && Input.GetKey(KeybindingsScript.Kb.uniqueAttack))
 		{
 			inputButton = 2;//Medium (B)
 		}
-		if (Input.GetKey(heavy) || Input.GetKey(heavy) && Input.GetKey(unique))
+		if (Input.GetKey(KeybindingsScript.Kb.heavyAttack) || 
+			Input.GetKey(KeybindingsScript.Kb.heavyAttack) && Input.GetKey(KeybindingsScript.Kb.uniqueAttack))
 		{
 			inputButton = 3;//Heavy (C)
 		}
-		if (Input.GetKey(unique) && !Input.GetKey(light) ||
-			Input.GetKey(unique) && !Input.GetKey(med) ||
-			Input.GetKey(unique) && !Input.GetKey(heavy))
+		if (Input.GetKey(KeybindingsScript.Kb.uniqueAttack) && !Input.GetKey(KeybindingsScript.Kb.lightAttack) ||
+			Input.GetKey(KeybindingsScript.Kb.uniqueAttack) && !Input.GetKey(KeybindingsScript.Kb.mediumAttack) ||
+			Input.GetKey(KeybindingsScript.Kb.uniqueAttack) && !Input.GetKey(KeybindingsScript.Kb.heavyAttack))
 		{
 			inputButton = 4;//Special (S)
 		}
-		if (Input.GetKey(light) && Input.GetKey(med) || 
-			Input.GetKey(light) && Input.GetKey(heavy) || 
-			Input.GetKey(med) && Input.GetKey(heavy))
+		if (Input.GetKey(KeybindingsScript.Kb.lightAttack) && Input.GetKey(KeybindingsScript.Kb.mediumAttack) || 
+			Input.GetKey(KeybindingsScript.Kb.lightAttack) && Input.GetKey(KeybindingsScript.Kb.heavyAttack) || 
+			Input.GetKey(KeybindingsScript.Kb.mediumAttack) && Input.GetKey(KeybindingsScript.Kb.heavyAttack))
 		{
 			inputButton = 5;//Throw
 		}
-		if (Input.GetKey(light) && Input.GetKey(med) && Input.GetKey(heavy))
+		if (Input.GetKey(KeybindingsScript.Kb.lightAttack) && 
+			Input.GetKey(KeybindingsScript.Kb.mediumAttack) && Input.GetKey(KeybindingsScript.Kb.heavyAttack))
 		{
 			inputButton = 6;//Psycho Cancel
 		}
-		if (Input.GetKey(light) && Input.GetKey(med) && Input.GetKey(heavy) && Input.GetKey(unique))
+		if (Input.GetKey(KeybindingsScript.Kb.lightAttack) && Input.GetKey(KeybindingsScript.Kb.mediumAttack) &&
+			Input.GetKey(KeybindingsScript.Kb.heavyAttack) && Input.GetKey(KeybindingsScript.Kb.uniqueAttack))
 		{
 			inputButton = 7;//Instant Kill
 		}
-		if (!Input.anyKeyDown)
+		if (!Input.GetKey(KeybindingsScript.Kb.lightAttack) || !Input.GetKey(KeybindingsScript.Kb.mediumAttack) ||
+			!Input.GetKey(KeybindingsScript.Kb.heavyAttack) || !Input.GetKey(KeybindingsScript.Kb.uniqueAttack))
 		{
 			inputButton = 0;
 		}
