cb2d3df64fbd18db486323978dec175c49ecfb8d
granthendrickson19 <31490789+granthendrickson19@users.noreply.github.com>
2018-11-08 09:37:32 -0800

    working on new hitbox detection

--- Battle Super Legends Super Edition/Assets/Scripts/combat Scripts/combat.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/combat Scripts/combat.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class combat : MonoBehaviour {
-	public Vector3 myLocation;
-	public Transform lightAttackPrefab;
+
+	public GameObject character1;
+	private Vector3 myLocation;
+	float timer = 0f;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		myLocation = GameObject.Find("Player").transform.position;
+		myLocation = character1.transform.position;
 		if(Input.GetKeyDown(KeybindingsScript.Kb.lightAttack)){
-			Instantiate(lightAttackPrefab, myLocation, Quaternion.identity);
+			character1.transform.GetChild(0).gameObject.SetActive(true); 
+			timer += 1f * Time.deltaTime;
+			if(timer >= .3f){
+				character1.transform.GetChild(0).gameObject.SetActive(false); 
+			}
+			timer =0f;
 		}
 	}
 }
