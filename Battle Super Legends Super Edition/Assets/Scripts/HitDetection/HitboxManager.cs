2ef11d69b5d4e76b66f83b5c177641d2c05b721b
Sebastian Miller <SMiller7644@student.pps.net>
2018-11-30 09:58:03 -0800

    adding hitbox support

--- Battle Super Legends Super Edition/Assets/Scripts/HitDetection/HitboxManager.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/HitDetection/HitboxManager.cs
@@ -3,44 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitboxManager : MonoBehaviour {
+    
+    BoxCollider2D hitBox;
+    float         xoffset;
+    Animator      animator;
 
-    //set in editor
-    public BoxCollider2D attackbox;
- 
-    //used for organization
-    private BoxCollider2D[] colliders;
- 
-    //collider on this game object
-    private BoxCollider2D localCollider;
- 
-    public enum hitBoxes
-    {
-        attackbox,
-        clear
-    }
-     
     void Start()
     {
-        //set hitbox array
-        colliders = new BoxCollider2D[]{attackbox};
- 
-        //create hitbox
-        localCollider = gameObject.AddComponent<BoxCollider2D>();
-        localCollider.isTrigger = true;//set as trigger so it doesn't collide with environment
-     }
+        animator = this.GetComponent<Animator>();
+        if (CombinedMove.Cm.moveDirection == 1)
+        {
+            xoffset = .64f;
+        } else if (CombinedMove.Cm.moveDirection == -1) {
+            xoffset = -.64f;
+        }
+        addBoxCollider2D("hitbox", new Vector2(.64f, 1.28f), new Vector2(xoffset, 0), true, false);
+    }
+
+    void Update()
+    {
+        
+    }
  
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("Collider hit something!");
+        Debug.Log("Collision Detected");
     }
- 
-    public void setHitBox(hitBoxes val)
+
+    public void activate()
     {
-        if(val != hitBoxes.clear)
-        {
-            //localCollider.SetPath(0, colliders[(int)val].GetPath(0));
-			localCollider.enabled = true;
-            return;
-        }
-     }
+        hitBox.enabled = true;
+        Debug.Log("Hitbox Activated");
+    }
+    public void deactivate()
+    {
+        hitBox.enabled = false;
+        Debug.Log("Hitbox Deactivated");
+    }
+    public void end()
+    {
+        animator.SetInteger("action", 0);
+        Debug.Log("Action set to 0");
+    }
+
+    private void addBoxCollider2D(string name, Vector2 size, Vector2 offset, bool boxType, bool enabled)
+    {
+        GameObject childbox = new GameObject(name);
+
+        childbox.transform.position = transform.position;
+        childbox.transform.SetParent(boxType ? transform.GetChild(1) : transform.GetChild(2));
+        
+        childbox.AddComponent<BoxCollider2D>();
+        childbox.GetComponent<BoxCollider2D>().size = size;
+        childbox.GetComponent<BoxCollider2D>().offset = offset;
+        childbox.GetComponent<BoxCollider2D>().enabled = enabled;
+        if (!boxType)
+            childbox.GetComponent<BoxCollider2D>().isTrigger = true;
+    }
 }
