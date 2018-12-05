cb0a9d93784810db4dc36c46cfff9aa20190014e
granthendrickson19 <31490789+granthendrickson19@users.noreply.github.com>
2018-12-05 13:52:40 -0800

    Random Generation Update

---
+++ Battle Super Legends Super Edition/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Generator : MonoBehaviour {
+
+	public List <int> spawnDirections = new List<int>();
+	public Sprite room1;
+	public Sprite room2;
+	public Sprite room2WithTop;
+	public Sprite room3;
+
+	public int numberOfRooms;
+
+	private int startRoomNum;
+	
+	private int endRoomNum; 
+	// Use this for initialization
+	void Start () {
+		numberOfRooms = Random.Range(11,20);
+
+		PlaceRooms();
+	}
+	
+	private void PlaceRooms(){
+		//0 is up
+		//1 is right
+		//2 is down
+		int nextRoomDirection; //sets to a random number between 0 and 2
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Battle Super Legends Super Edition/Assets/Scripts/Map_Generator.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/Map_Generator.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map_Generator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour {
 
-	public int[,] Rooms = new int[3,8];
+	public List <int> spawnDirections = new List<int>();
+	public List <string> roomExitType = new List<string>();	
 	public Sprite room1;
 	public Sprite room2;
 	public Sprite room2WithTop;
@@ -19,15 +20,55 @@ public class Map_Generator : MonoBehaviour {
 	void Start () {
 		numberOfRooms = Random.Range(11,20);
 
-		PlaceRooms();
+		GenerateMap();
+		AssignRoomCode();
 	}
 	
-	private void PlaceRooms(){
+	private void GenerateMap(){
 		//0 is up
 		//1 is right
 		//2 is down
-		int nextRoomDirection; //sets to a random number between 0 and 2
+		int nextRoomDirection;  //sets to a random number between 0 and 2
+		int previousRoomDirection = -1;
+		for(int i = 0; i < numberOfRooms; i++){
+			if(previousRoomDirection == 0){
+				nextRoomDirection = Random.Range(0,2);
+			}
+			else if (previousRoomDirection == 2){
+				nextRoomDirection = Random.Range(1,3);
+			}
+			else {
+				nextRoomDirection = Random.Range(0,3);
+			}
+			previousRoomDirection = nextRoomDirection;
+			Debug.Log(nextRoomDirection);
+			spawnDirections.Add(nextRoomDirection);
+			
 
+		}
+
+	}
+
+	private void AssignRoomCode() {
+		roomExitType[0] = "Z";
+		roomExitType[numberOfRooms-1] = "Y";
+		for (int i = 1; i < numberOfRooms-1; i++){
+			if(spawnDirections[i-1] == 0 && spawnDirections[i+1] == 0){
+				roomExitType[i] = "A";
+			}
+			else if(spawnDirections[i-1] == 0 && spawnDirections[i+1] == 1){
+				roomExitType[i] = "D";
+			}
+			else if(spawnDirections[i-1] == 1 && spawnDirections[i+1] == 0){
+				roomExitType[i] = "F";
+			}
+			else if(spawnDirections[i-1] == 1 && spawnDirections[i+1] == 1){
+				roomExitType[i] = "C";
+			}
+			else if (spawnDirections[i-1] == 1 && spawnDirections[i+1] ==2){
+				roomExitType[i] = "E";
+			}
+		}
 	}
 	// Update is called once per frame
 	void Update () {
