f30a54e5042948485888ab7741457a6ed33f2b75
granthendrickson19 <31490789+granthendrickson19@users.noreply.github.com>
2018-12-10 08:40:19 -0800

    YUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUh

--- Battle Super Legends Super Edition/Assets/Scripts/MapGenerator.cs
+++ Battle Super Legends Super Edition/Assets/Scripts/MapGenerator.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map_Generator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour {
 
 	public List <int> spawnDirections = new List<int>();
+	public List <string> roomExitType = new List<string>();	
 	public Sprite room1;
 	public Sprite room2;
 	public Sprite room2WithTop;
@@ -17,19 +18,110 @@ public class Map_Generator : MonoBehaviour {
 	private int endRoomNum; 
 	// Use this for initialization
 	void Start () {
+		
 		numberOfRooms = Random.Range(11,20);
-
+		Debug.Log("Number of Rooms = " + numberOfRooms);
+		GenerateMap();
+		AssignRoomCode();
 		PlaceRooms();
 	}
 	
-	private void PlaceRooms(){
+	private void GenerateMap(){
 		//0 is up
 		//1 is right
 		//2 is down
-		int nextRoomDirection; //sets to a random number between 0 and 2
+		int nextRoomDirection;  //sets to a random number between 0 and 2
+		int previousRoomDirection = -1;
+		for(int i = 0; i < numberOfRooms; i++){
+			if(previousRoomDirection == 0){
+				nextRoomDirection = Random.Range(0,2);
+			}
+			else if (previousRoomDirection == 2){
+				nextRoomDirection = Random.Range(1,3);
+			}
+			else {
+				nextRoomDirection = Random.Range(0,3);
+			}
+			previousRoomDirection = nextRoomDirection;
+			Debug.Log(nextRoomDirection);
+			spawnDirections.Add(nextRoomDirection);
+			
+
+		}
 
 	}
-	// Update is called once per frame
+
+	private void AssignRoomCode() {
+		roomExitType.Add("Z");
+		for (int i = 1; i < numberOfRooms-1; i++){
+
+			if(spawnDirections[i-1] == 0 && spawnDirections[i+1] == 0 && spawnDirections[i] == 0){
+				roomExitType.Add("A");
+			}
+			else if (spawnDirections[i-1] == 2 && spawnDirections[i+1] == 2 && spawnDirections[i] == 0){
+				roomExitType.Add("A");
+			}
+			else if (spawnDirections[i-1] == 1 && spawnDirections[i+1] == 0 && spawnDirections[i] == 0){
+				roomExitType.Add("A");
+			}
+
+
+
+			else if (spawnDirections[i-1] == 2 && spawnDirections[i+1] == 1 && spawnDirections[i] == 2) {
+				roomExitType.Add("B");
+			}
+			else if (spawnDirections[i-1] == 1 && spawnDirections[i+1] == 1 && spawnDirections[i] == 2){
+				roomExitType.Add("B");
+			}
+
+
+
+			else if(spawnDirections[i-1] == 1 && spawnDirections[i+1] == 1 && spawnDirections[i] == 1){
+				roomExitType.Add("C");
+			}
+			else if(spawnDirections[i-1] == 0 && spawnDirections[i+1] == 1 && spawnDirections[i] == 1){
+				roomExitType.Add("C");
+			}
+			else if(spawnDirections[i-1] == 2 && spawnDirections[i+1] == 1 && spawnDirections[i] == 1){
+				roomExitType.Add("C");
+			}
+
+
+
+			else if(spawnDirections[i-1] == 0 && spawnDirections[i+1] == 1 && spawnDirections[i] == 0){
+				roomExitType.Add("D");
+			}
+			else if(spawnDirections[i-1] == 1 && spawnDirections[i+1] == 1 && spawnDirections[i] == 0){
+				roomExitType.Add("D");
+			}
+
+
+
+			else if (spawnDirections[i-1] == 1 && spawnDirections[i+1] == 2 && spawnDirections[i] == 1){
+				roomExitType.Add("E");
+			}
+			else if (spawnDirections[i-1] == 2 && spawnDirections[i+1] == 2 && spawnDirections[i] == 1){
+				roomExitType.Add("E");
+			}
+
+
+
+			else if(spawnDirections[i-1] == 1 && spawnDirections[i+1] == 0 && spawnDirections[i] == 1){
+				roomExitType.Add("F");
+			}
+			else if(spawnDirections[i-1] == 0 && spawnDirections[i+1] == 0 && spawnDirections[i] == 1){
+				roomExitType.Add("F");
+			}
+		}
+		roomExitType.Add("Y");
+		foreach(string x in roomExitType){
+			Debug.Log(x);
+		}
+	}
+	
+	private void PlaceRooms(){
+		
+	}
 	void Update () {
 		
 	}
